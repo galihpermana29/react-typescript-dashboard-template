e790637f9c402a4d30aa408636b74098427325c6
Galih Permana <101629052+galihpermana292@users.noreply.github.com>
2024-04-30 09:56:18 +0700

    initial rules

--- src/shared/usecase/useRenderRoutes.ts
+++ src/shared/usecase/useRenderRoutes.ts
@@ -4,6 +4,7 @@ import useMapRoutes from './useMapRoutes';
 function generateRoutesChild() {
 	const { mappingRoutes, getUserType } = useMapRoutes();
 	const action = getUserType();
+
 	// TODO: roles switcher if we want render different type of user/roles
 	const whichData = action === 'staff' ? staffRoutes : staffRoutes;
 	const renderedRoutes = mappingRoutes(whichData);
