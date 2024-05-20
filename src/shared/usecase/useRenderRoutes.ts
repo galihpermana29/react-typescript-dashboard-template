c5cf7ce7679161de814c55e8cd1616da7a68e776
galihpermana29 <101629052+galihpermana292@users.noreply.github.com>
2024-05-20 20:46:27 +0700

    hotfix: reload type

--- src/shared/usecase/useRenderRoutes.ts
+++ src/shared/usecase/useRenderRoutes.ts
@@ -8,10 +8,13 @@ import useMapRoutes from './useMapRoutes';
 function generateRoutesChild() {
 	const { mappingRoutes } = useMapRoutes();
 
+	// TODO: change this logic once be ready
 	const userType: actionType | null = JSON.parse(
 		localStorage.getItem('type_user')!
 	) as actionType;
-	if (!userType) window.location.reload();
+	const isLoggedIn = JSON.parse(localStorage.getItem('token')!);
+
+	if (!userType && isLoggedIn) window.location.reload();
 	const whichData = userType === 'admin' ? staffRoutes : vendorRoutes;
 
 	const renderedRoutes = mappingRoutes(whichData);
