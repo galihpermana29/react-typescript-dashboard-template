83aed25bb8afcd71d5c693e00b1411b3a9e15c39
galihpermana29 <101629052+galihpermana292@users.noreply.github.com>
2024-05-30 00:16:51 +0700

    hotifx: user management queryKey

--- src/routes/admin/vendor-management/vendor-master-data/respositories/tag/useGetAllTags.tsx
+++ src/routes/admin/vendor-management/vendor-master-data/respositories/tag/useGetAllTags.tsx
@@ -9,79 +9,79 @@ import { useQuery } from 'react-query';
 import { useSearchParams } from 'react-router-dom';
 
 const useQueryProductTags = (form: FormInstance<any>) => {
-	const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-	const keyword = searchParams.get('keyword');
-	const status = searchParams.get('status');
-	const limit = searchParams.get('limit');
-	const page = searchParams.get('page');
+  const keyword = searchParams.get('keyword');
+  const status = searchParams.get('status');
+  const limit = searchParams.get('limit');
+  const page = searchParams.get('page');
 
-	// Default filter state
-	const initialFilterState: TGeneralFilter = {
-		limit: 10,
-		page: 1,
-		keyword: '',
-		status: 'default',
-	};
+  // Default filter state
+  const initialFilterState: TGeneralFilter = {
+    limit: 10,
+    page: 1,
+    keyword: '',
+    status: 'default',
+  };
 
-	const [queryProductTags, setQueryProductTags] = useState<TGeneralFilter>({
-		limit: limit ? parseInt(limit) : initialFilterState.limit,
-		page: page ? parseInt(page) : initialFilterState.page,
-		keyword: keyword ?? initialFilterState.keyword,
-		status: status ?? initialFilterState.status,
-	});
+  const [queryProductTags, setQueryProductTags] = useState<TGeneralFilter>({
+    limit: limit ? parseInt(limit) : initialFilterState.limit,
+    page: page ? parseInt(page) : initialFilterState.page,
+    keyword: keyword ?? initialFilterState.keyword,
+    status: status ?? initialFilterState.status,
+  });
 
-	const { objectToQueryParams } = useConvertQuery();
-	const { addIndexToData } = useSuccessAxios();
-	const queries = useDebounce(queryProductTags, 1000);
+  const { objectToQueryParams } = useConvertQuery();
+  const { addIndexToData } = useSuccessAxios();
+  const queries = useDebounce(queryProductTags, 1000);
 
-	const getProducTags = async () => {
-		const queryParams = objectToQueryParams(queryProductTags);
-		setSearchParams(queryParams);
+  const getProducTags = async () => {
+    const queryParams = objectToQueryParams(queryProductTags);
+    setSearchParams(queryParams);
 
-		const { data, meta_data } = await DashboardProductAPI.getAllProductTags(
-			queryParams
-		);
+    const { data, meta_data } = await DashboardProductAPI.getAllProductTags(
+      queryParams
+    );
 
-		return { data: addIndexToData(data), meta_data };
-	};
+    return { data: addIndexToData(data), meta_data };
+  };
 
-	const { data, error, isLoading, refetch } = useQuery({
-		queryKey: ['vendor', { ...queries }],
-		queryFn: getProducTags,
-	});
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['tags', { ...queries }],
+    queryFn: getProducTags,
+  });
 
-	const handleFilter = (value: any) => {
-		for (const x in value) {
-			if (value[x]) {
-				setQueryProductTags((val) => ({ ...val, [x]: value[x] }));
-			}
-		}
-	};
+  const handleFilter = (value: any) => {
+    for (const x in value) {
+      if (value[x]) {
+        setQueryProductTags((val) => ({ ...val, [x]: value[x] }));
+      }
+    }
+  };
 
-	const clearFilter = () => {
-		const clearFilterQuery = {
-			...initialFilterState,
-			limit: queries.limit,
-			page: queries.page,
-		};
-		form.setFieldsValue(clearFilterQuery);
+  const clearFilter = () => {
+    const clearFilterQuery = {
+      ...initialFilterState,
+      limit: queries.limit,
+      page: queries.page,
+    };
+    form.setFieldsValue(clearFilterQuery);
 
-		setQueryProductTags(() => ({
-			...clearFilterQuery,
-		}));
-	};
+    setQueryProductTags(() => ({
+      ...clearFilterQuery,
+    }));
+  };
 
-	return {
-		data,
-		error,
-		isLoading,
-		refetch,
-		setQueryProductTags,
-		queryProductTags,
-		handleFilter,
-		clearFilter,
-	};
+  return {
+    data,
+    error,
+    isLoading,
+    refetch,
+    setQueryProductTags,
+    queryProductTags,
+    handleFilter,
+    clearFilter,
+  };
 };
 
 export default useQueryProductTags;
