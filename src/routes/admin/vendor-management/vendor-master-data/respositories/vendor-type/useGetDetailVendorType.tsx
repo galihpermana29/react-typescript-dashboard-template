d7f9057b2374d6bdb390f1271df22ca12ce6ca82
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-05-29 23:29:24 +0700

    fix: Change DashboardProductAPI to DashboardVendorTypeAPI

--- src/routes/admin/vendor-management/vendor-master-data/respositories/vendor-type/useGetDetailVendorType.tsx
+++ src/routes/admin/vendor-management/vendor-master-data/respositories/vendor-type/useGetDetailVendorType.tsx
@@ -1,38 +1,36 @@
-import { DashboardProductAPI } from "@/shared/repositories/productService";
-import { FormInstance } from "antd";
-import { TModalState } from "../../usecase/useModalReducer";
-import { useQuery } from "react-query";
-
+import { DashboardVendorTypeAPI } from '@/shared/repositories/vendorTypeService';
+import { FormInstance } from 'antd';
+import { TModalState } from '../../usecase/useModalReducer';
+import { useQuery } from 'react-query';
 
 const useQueryVendorTypeDetail = (
-    modalState?: TModalState,
-    form?: FormInstance<any>
+  modalState?: TModalState,
+  form?: FormInstance<any>
 ) => {
+  const getDetail = async () => {
+    const { data } = await DashboardVendorTypeAPI.getVendorTypeDetail(
+      modalState?.id as string
+    );
 
-    const getDetail = async () => {
-        const { data } = await DashboardProductAPI.getVendorTypeDetail(
-            modalState?.id as string
-        );
-
-        form!.setFieldsValue({
-            ...data,
-        });
-
-        return data;
-    };
-
-    const { data, error, isLoading, refetch } = useQuery({
-        queryKey: ['vendor-vendor-type-detail', modalState?.id],
-        queryFn: getDetail,
-        enabled: modalState?.id ? true : false,
+    form!.setFieldsValue({
+      ...data,
     });
 
-    return {
-        data,
-        error,
-        isLoading,
-        refetch
-    };
-}
+    return data;
+  };
+
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['vendor-vendor-type-detail', modalState?.id],
+    queryFn: getDetail,
+    enabled: modalState?.id ? true : false,
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+    refetch,
+  };
+};
 
 export default useQueryVendorTypeDetail;
