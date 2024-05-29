88452e464d64610e497082a78a452218edc369d7
galihpermana29 <101629052+galihpermana292@users.noreply.github.com>
2024-05-30 00:13:28 +0700

    hotifx: user management queryKey

--- src/routes/admin/user-management/UserManagement.tsx
+++ src/routes/admin/user-management/UserManagement.tsx
@@ -12,71 +12,70 @@ import { AxiosError } from 'axios';
 import useMutateEditClientUser from './repositories/useUpdateUser';
 
 export const UserManagementContainer = () => {
-	const [form] = useForm();
+  const [form] = useForm();
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const {
-		data,
-		queryClientUser,
-		setQueryClientUser,
-		error,
-		refetch,
-		handleFilter,
-		clearFilter,
-		isLoading: loadingGetAll,
-	} = useQueryClientUser(form);
+  const {
+    data,
+    queryClientUser,
+    setQueryClientUser,
+    error,
+    refetch,
+    handleFilter,
+    clearFilter,
+    isLoading: loadingGetAll,
+  } = useQueryClientUser(form);
 
-	const { permissions } = useLoaderData() as ILoaderData;
-	const { view, edit, remove } = permissions;
+  const { permissions } = useLoaderData() as ILoaderData;
+  const { view, edit, remove } = permissions;
 
-	const { mutate: mutateEdit } = useMutateEditClientUser(refetch);
+  const { mutate: mutateEdit } = useMutateEditClientUser(refetch);
 
-	const { columns } = useGenerateColumn(
-		remove,
-		edit,
-		view,
-		navigate,
-		mutateEdit
-	);
-
-	return (
-		<ErrorBoundary error={error as AxiosError} refetch={refetch}>
-			<TableHeaderTitle title="User Management" />
-			<DashboardTable
-				columns={columns}
-				data={data?.data}
-				onPaginationChanges={setQueryClientUser}
-				loading={loadingGetAll}
-				filterComponents={
-					<DashboardTableFilter
-						form={form}
-						queryAdmins={queryClientUser}
-						onApplyFilter={handleFilter}
-						onClearFilter={clearFilter}
-						onSearch={setQueryClientUser}
-						buttonComponents={undefined}
-						filterComponents={
-							<>
-								<Form.Item
-									name={'status'}
-									label="Status"
-									initialValue={queryClientUser.status}
-									className="my-[10px]">
-									<Select
-										className="h-[35px]"
-										options={[
-											{ value: 'default', label: 'All' },
-											{ value: 'active', label: 'Active' },
-											{ value: 'inactive', label: 'Inactive' },
-										]}
-									/>
-								</Form.Item>
-							</>
-						}
-					/>
-				}
-			/>
-		</ErrorBoundary>
-	);
+  const { columns } = useGenerateColumn(
+    remove,
+    edit,
+    view,
+    navigate,
+    mutateEdit
+  );
+  return (
+    <ErrorBoundary error={error as AxiosError} refetch={refetch}>
+      <TableHeaderTitle title="User Management" />
+      <DashboardTable
+        columns={columns}
+        data={data?.data}
+        onPaginationChanges={setQueryClientUser}
+        loading={loadingGetAll}
+        filterComponents={
+          <DashboardTableFilter
+            form={form}
+            queryAdmins={queryClientUser}
+            onApplyFilter={handleFilter}
+            onClearFilter={clearFilter}
+            onSearch={setQueryClientUser}
+            buttonComponents={undefined}
+            filterComponents={
+              <>
+                <Form.Item
+                  name={'status'}
+                  label="Status"
+                  initialValue={queryClientUser.status}
+                  className="my-[10px]">
+                  <Select
+                    className="h-[35px]"
+                    options={[
+                      { value: 'default', label: 'All' },
+                      { value: 'active', label: 'Active' },
+                      { value: 'inactive', label: 'Inactive' },
+                    ]}
+                  />
+                </Form.Item>
+              </>
+            }
+          />
+        }
+      />
+    </ErrorBoundary>
+  );
 };
