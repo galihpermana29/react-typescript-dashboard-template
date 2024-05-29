dc29eb6880d8f5a9a12a678e3f60242b716d33d2
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-29 12:18:24 +0700

    fix: revert removing getAllRoles query by accident

--- src/shared/repositories/roleServies.tsx
+++ src/shared/repositories/roleServies.tsx
@@ -1,4 +1,5 @@
 import {
+	IAllRolesResponseRoot,
 	ICreateRolePayloadRoot,
 	ICreateRoleResponseRoot,
 	IDetailRoleResponseRoot,
@@ -10,6 +11,17 @@ import { ApiClass } from './generalApi';
 class DashboardRoleServices extends ApiClass {
 	constructor(baseURL?: string, config?: Record<string, any>) {
 		super(baseURL, config);
+	}
+
+	public async getAllRoles(query?: string): Promise<IAllRolesResponseRoot> {
+		const token = JSON.parse(localStorage.getItem('token')!);
+
+		const { data } = await this.axiosInstance.get<IAllRolesResponseRoot>(
+			`/roles${query ? `?${query}` : ''}`,
+			{ headers: { Authorization: `Bearer ${token}` } }
+		);
+
+		return data;
 	}
 
 	public async getRoleById(id: string): Promise<IDetailRoleResponseRoot> {
